92e18389d949b2fc27b536e586b2bcb846c55f1c
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-17 20:22:04 -0300

    Add basic functions and TODO entries

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-console.log("Hello");
+// TODO: Add a button for deleting book entries
+// TODO: Add a button for toggling its read status
+
+
+const myLibrary = [];
+const bookshelf = document.querySelector(".bookshelf");
+const newBookButton = document.querySelector(".new-book");
+
+function Book(title, author, pages, releaseYear, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.releaseYear = releaseYear;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // TODO: Adds books to the library
+}
+
+function displayEachBook(bookArray) {
+    for (const book of bookArray) {
+        // TODO: display books in DOM
+    }
+}
+
+newBookButton.addEventListener('click', () => {
+    // TODO: add form for inserting new books
+});
+
+displayEachBook(bookshelf);
