e5724c16d48af242d6dc460e708c4a7e2021e3b3
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-17 20:47:21 -0300

    Add book entry form and new book button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -26,6 +26,21 @@ function displayEachBook(bookArray) {
 
 newBookButton.addEventListener('click', () => {
     // TODO: add form for inserting new books
+    const bookForm = document.querySelector(".book-form");
+    if (bookForm.style.display == 'none') {
+        bookForm.style.display = 'block'; // Maybe change this
+    }
+    else {
+        bookForm.style.display = 'none';
+    }
+    
 });
+
+// Dummy data
+let book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 300, 1925);
+let book2 = new Book("To Kill a Mockingbird", "Harper Lee", 350, 1960, true);
+let book3 = new Book("1984", "George Orwell", 400, 1949);
+let book4 = new Book("The Catcher in the Rye", "J.D. Salinger", 240, 1951, true);
+let book5 = new Book("Pride and Prejudice", "Jane Austen", 320, 1813);
 
 displayEachBook(bookshelf);
